fb4426ea489a0eccf8501786b814a00cfce01eb6
Denz Del Villar <denz.delvillar@tum-create.edu.sg>
2023-04-15 06:55:27 +0800

    add flag counter on map page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -70,7 +70,7 @@ const Home = () => {
     <br></br>
 
     <div className="flag-counter">
-    <a  href="https://info.flagcounter.com/s6dX"><img src="https://s11.flagcounter.com/count2/s6dX/bg_FFFFFF/txt_000000/border_CCCCCC/columns_4/maxflags_12/viewers_3/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"/></a>
+    <a  href="https://info.flagcounter.com/s6dX"><img src="https://s11.flagcounter.com/count2/s6dX/bg_FFFFFF/txt_000000/border_CCCCCC/columns_4/maxflags_16/viewers_3/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"/></a>
     </div>
     </section>
     );
