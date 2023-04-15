177bd391e7c8132f5971b76fb7234b6ca64c4caf
Denz Del Villar <denz.delvillar@tum-create.edu.sg>
2023-04-15 22:46:37 +0800

    reduce size of images, remove unnecessary code

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -33,7 +33,8 @@ function MapPage() {
                 </section>
                 <section className="save-image-button" onClick={downloadJpg}>Save Image</section>
             </section>
-        <AdsComponent dataAdSlot='8658257981' /> 
+        <div id="ezoic-pub-ad-placeholder-103"> </div>
+        {/*<AdsComponent dataAdSlot='8658257981' /> */}
         <div className="flag-counter">
             <a  href="https://info.flagcounter.com/s6dX">
                 <img src="https://s11.flagcounter.com/count2/s6dX/bg_FFFFFF/txt_000000/border_CCCCCC/columns_4/maxflags_16/viewers_3/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"/>
