1cca2cf5cb64dfbc25090186d8c48b149aef269f
Gerald Ballesteros <geraldballesteros@outlook.com>
2023-04-21 18:32:59 +0800

    #18 ' to "

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,11 +1,11 @@
-import React, {  useCallback, useRef } from "react";
+import React, {  useCallback, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { toJpeg } from 'html-to-image'
 
-import Map from "./Map"
+import Map from './Map'
 import FacebookLogo from '../components/FacebookLogo'
 import { FB_SHARE_URL } from '../utils/constants'
-import ".././App.css"
+import '.././App.css'
 
 function MapPage() {
     const divRef = useRef(null);
@@ -36,29 +36,29 @@ function MapPage() {
 
     return (
         <section>
-            <div id="ezoic-pub-ad-placeholder-105"> </div>
-            <section className="Map-container">
-                <section className="Map-padding" ref={divRef}>
+            <div id='ezoic-pub-ad-placeholder-105'> </div>
+            <section className='Map-container'>
+                <section className='Map-padding' ref={divRef}>
                     <Map/>
                 </section>
-                <section className="save-image-button" onClick={downloadJpg}>Save Image</section>
+                <section className='save-image-button' onClick={downloadJpg}>Save Image</section>
                 <section className='share-fb-button' onClick={handleFacebookShare}>
                   <FacebookLogo />
                   Share
                 </section>
             </section>
 
-    <section className="donate">
+    <section className='donate'>
     If you liked this project and want it to be accessible in the future, please consider donating below. Thank you!
 
     <br></br>
     <br></br>
 
-    <div className="donate-button">
-   <form action="https://www.paypal.com/donate" method="post" target="_top">
-    <input type="hidden" name="hosted_button_id" value="RP4T6EPM6GJ9A" />
-    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-    <img alt="" border="0" src="https://www.paypal.com/en_PH/i/scr/pixel.gif" width="1" height="1" />
+    <div className='donate-button'>
+   <form action='https://www.paypal.com/donate' method='post' target='_top'>
+    <input type='hidden' name='hosted_button_id' value='RP4T6EPM6GJ9A' />
+    <input type='image' src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif' border='0' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button' />
+    <img alt='' border='0' src='https://www.paypal.com/en_PH/i/scr/pixel.gif' width='1' height='1' />
     </form>
     </div>
 
@@ -66,11 +66,11 @@ function MapPage() {
     <br></br>
 
     </section>
-        <div id="ezoic-pub-ad-placeholder-103"> </div>
+        <div id='ezoic-pub-ad-placeholder-103'> </div>
         {/*<AdsComponent dataAdSlot='8658257981' /> */}
-        <div className="flag-counter">
-            <a  href="https://info.flagcounter.com/s6dX">
-                <img src="https://s11.flagcounter.com/count2/s6dX/bg_FFFFFF/txt_000000/border_CCCCCC/columns_4/maxflags_16/viewers_3/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"/>
+        <div className='flag-counter'>
+            <a  href='https://info.flagcounter.com/s6dX'>
+                <img src='https://s11.flagcounter.com/count2/s6dX/bg_FFFFFF/txt_000000/border_CCCCCC/columns_4/maxflags_16/viewers_3/labels_0/pageviews_1/flags_0/percent_0/' alt='Flag Counter' border='0'/>
             </a>
         </div>
         </section>
