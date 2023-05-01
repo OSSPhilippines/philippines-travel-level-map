80511144a2d71e79153e996ede81536b1dea155e
Joff Tiquez <jofftiquez@gmail.com>
2023-05-01 14:29:49 +0800

    🎨 style(index.js): fix indentation and add comma after React.StrictMode 🚀 chore(Layout.js): add React import and format code 🚀 chore(NoPage.js): add React import and format code The changes in index.js are purely cosmetic, fixing indentation and adding a comma to the React.StrictMode component. In Layout.js and NoPage.js, the code has been formatted to improve readability and consistency. The React import has also been added to both files.
    🎨 style(levelConverter.js): format code with consistent indentation and spacing
    This commit only changes the indentation and spacing of the code in levelConverter.js to make it more consistent and easier to read. There are no functional changes to the code.
    
    ✅ test(levelConverter.test.js): improve readability of test cases
    The changes in this commit are purely cosmetic and do not affect the functionality of the code. The test cases have been reformatted to improve readability and consistency.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,21 @@
-import { Outlet, Link } from "react-router-dom";
-import ".././App.css"
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import '.././App.css';
 
 const Layout = () => {
-  return (
-    <section className="nav">
-      <nav className="nav">
-        <ul  className="nav-ul">
-          <li className="nav">
-            <Link className="nav" to="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
+	return (
+		<section className="nav">
+			<nav className="nav">
+				<ul  className="nav-ul">
+					<li className="nav">
+						<Link className="nav" to="/">Home</Link>
+					</li>
+				</ul>
+			</nav>
 
-      <Outlet />
-    </section>
-  )
+			<Outlet />
+		</section>
+	);
 };
 
 export default Layout;
--- src/utils/levelConverter.js
+++ src/utils/levelConverter.js
@@ -1,25 +1,25 @@
 import { PROVINCES_LENGTH } from './constants';
 
 const levelStringToArray = (levelStr) => {
-  const levelString = levelStr ?? '';
-  const levelRegex = /^[0-5]+$/;
-  if (
-    levelString.length !== PROVINCES_LENGTH ||
+	const levelString = levelStr ?? '';
+	const levelRegex = /^[0-5]+$/;
+	if (
+		levelString.length !== PROVINCES_LENGTH ||
     !levelString.match(levelRegex)
-  ) {
-    return new Array(PROVINCES_LENGTH).fill(0);
-  }
+	) {
+		return new Array(PROVINCES_LENGTH).fill(0);
+	}
 
-  const arr = levelString.split('');
-  return arr.map((provinceLevel) => parseInt(provinceLevel));
+	const arr = levelString.split('');
+	return arr.map((provinceLevel) => parseInt(provinceLevel));
 };
 
 const levelArrayToString = (levelArr) => {
-  if (levelArr.length !== PROVINCES_LENGTH) {
-    return new Array(PROVINCES_LENGTH).fill('0').join('');
-  }
+	if (levelArr.length !== PROVINCES_LENGTH) {
+		return new Array(PROVINCES_LENGTH).fill('0').join('');
+	}
 
-  return levelArr.join('');
+	return levelArr.join('');
 };
 
 export { levelStringToArray, levelArrayToString };
