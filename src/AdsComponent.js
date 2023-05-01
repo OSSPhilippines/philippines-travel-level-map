f0566ef14c2814127ff75fb3c629d109b4297448
Joff Tiquez <jofftiquez@gmail.com>
2023-05-01 14:28:10 +0800

    🎨 style(AdsComponent.js): clean up code formatting and remove unnecessary lines 💄 style(AdsComponent.js): update class to className for consistency The code formatting has been cleaned up and unnecessary lines have been removed to improve readability. The class attribute has been updated to className for consistency with React's naming conventions.
    🎨 style(App.js): fix indentation and whitespace
    🧪 test(App.test.js): refactor test to use arrow function and remove unused imports
    The changes in App.js are purely cosmetic, fixing indentation and whitespace to improve readability. In App.test.js, the test function has been refactored to use an arrow function and unused imports have been removed.
    
    🔥 refactor(AppRouter.js): remove commented out code
    The commented out code for the Map component has been removed from the AppRouter.js file. This improves the readability of the code and removes unnecessary clutter.

--- src/AdsComponent.js
+++ src/AdsComponent.js
@@ -1,34 +1,24 @@
 import React, { useEffect  } from 'react';
 
 const AdsComponent = () => {
+	useEffect(() => {
+		try {
+			(window.adsbygoogle = window.adsbygoogle || []).push({});
+		} catch (err) {
+			console.log(err);
+		}
+	},[]);
 
-
-
-    useEffect(() => {
-
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-
-        catch (e) {
-
-        }
-
-    },[]);
-
-
-
-    return (
-        <>
-        
-        <ins class="adsbygoogle"
-     style={{display:'block'}}
-     data-ad-client="ca-pub-1919396112849413"
-     data-ad-slot="8658257981"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-        </>
-    );
+	return (
+		<>
+			<ins className="adsbygoogle"
+				style={{display:'block'}}
+				data-ad-client="ca-pub-1919396112849413"
+				data-ad-slot="8658257981"
+				data-ad-format="auto"
+				data-full-width-responsive="true"></ins>
+		</>
+	);
 };
 
 export default AdsComponent;
--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import MapPage from "./pages/MapPage";
-import NoPage from "./pages/NoPage";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import MapPage from './pages/MapPage';
+import NoPage from './pages/NoPage';
 
-function App() {
-  return (
-    <div className="App">
-      <div id="ezoic-pub-ad-placeholder-110"> </div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="map" element = {<MapPage />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-      {/*<div className="Map-container">
+function App () {
+	return (
+		<div className="App">
+			<div id="ezoic-pub-ad-placeholder-110"> </div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="map" element = {<MapPage />} />
+						<Route path="*" element={<NoPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+			{/*<div className="Map-container">
         <div className="Map-padding" ref={divRef}>
         <Map/>
         </div>
         <div className="save-image-button" onClick={downloadJpg}>Save Image</div>
       </div>
   <AdsComponent dataAdSlot='8658257981' />*/}
-  </div>
-  );
+		</div>
+	);
 }
 
 export default App;
