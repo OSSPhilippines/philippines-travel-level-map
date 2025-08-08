95aa645d350ca950fb1b9de7b4433e5714376cd5
Gerald <dev@xyz369.simplelogin.com>
2025-08-08 04:32:12 +0100

    chore: add vitest for testing

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
 	build: {
 		outDir: 'build', // CRA's default build output
 	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/setupTests.js',
+	},
 });
